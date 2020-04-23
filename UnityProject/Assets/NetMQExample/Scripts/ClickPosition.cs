21796effc53bb997ab6e3a5aaf8fd637e761b72b
Paul Zhou <paul_zhou@brown.edu>
2020-04-22 21:54:10 -0400

    point vector and angle theta

--- UnityProject/Assets/NetMQExample/Scripts/ClickPosition.cs
+++ UnityProject/Assets/NetMQExample/Scripts/ClickPosition.cs
@@ -29,12 +29,33 @@ public class ClickPosition : MonoBehaviour
                 clickPosition = hit.point;
             }
 
+            // getting the latitude and longitude 
             float lonRescale = (float) (maxLon - minLon) / 10;
             float latRescale = (float) (maxLat - minLat) / 10;
             float newLon = (float) (clickPosition.x * lonRescale + 0.5 * (minLon + maxLon));
             float newLat = (float) (clickPosition.y * latRescale + 0.5 * (minLat + maxLat)); 
 
-            Vector3 mapPosition = new Vector3(newLon, newLat, 0); 
+            // the angle of the vector from the positive y axis 
+            float theta;
+            if (clickPosition.y > 0) 
+            {
+                if (clickPosition.x < 0) 
+                {
+                    theta = (float) (Mathf.Atan(clickPosition.x / clickPosition.y) + 6.28);    
+                }
+                else 
+                {
+                    theta = (float) Mathf.Atan(clickPosition.x / clickPosition.y);
+                }
+            }
+            else 
+            {
+                theta = (float) (Mathf.Atan(clickPosition.x / clickPosition.y) + 3.14) ;
+            };
+
+            // float newX = (float) (clickPosition.x * lonRescale);
+            // float newY = (float) (clickPosition.y * latRescale);
+            // float normalize = (float) (1 / (newX * newX + newY * newY));
 
             // Method 2: Raycast using Plane 
             // Plane plane = new Plane(Vector3.up, 0f);
@@ -49,6 +70,8 @@ public class ClickPosition : MonoBehaviour
             Debug.Log("click position is" + clickPosition);
             Debug.Log("longitutde is " + newLon);
             Debug.Log("latitude is " + newLat);
+            Debug.Log("point vector is" + (clickPosition.x, clickPosition.y));
+            Debug.Log("angle theta is" + theta);
         }
     }
 }
