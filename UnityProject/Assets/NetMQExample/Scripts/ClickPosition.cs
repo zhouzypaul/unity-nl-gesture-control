419445bb0aadc43a9f7e8a5dbbc3031cd8e17701
Paul Zhou <paul_zhou@brown.edu>
2020-04-26 00:23:27 -0400

    find the nearest building

--- UnityProject/Assets/NetMQExample/Scripts/ClickPosition.cs
+++ UnityProject/Assets/NetMQExample/Scripts/ClickPosition.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class ClickPosition : MonoBehaviour
 {
-    public float minLat;
-    public float minLon;
-    public float maxLat;
-    public float maxLon;
+    public double minLat;
+    public double minLon;
+    public double maxLat;
+    public double maxLon;
+    public double theta; 
+    public double slope;
 
-
+    public LayerMask clickMask;
 
     // Update is called once per frame
     void Update()
@@ -24,34 +26,35 @@ public class ClickPosition : MonoBehaviour
             // remember to tag the camera as main camera!!!
             RaycastHit hit;
 
-            if (Physics.Raycast (ray, out hit))
+            if (Physics.Raycast (ray, out hit, 100f, clickMask))
             {
                 clickPosition = hit.point;
             }
 
             // getting the latitude and longitude 
-            float lonRescale = (float) (maxLon - minLon) / 10;
-            float latRescale = (float) (maxLat - minLat) / 10;
-            float newLon = (float) (clickPosition.x * lonRescale + 0.5 * (minLon + maxLon));
-            float newLat = (float) (clickPosition.y * latRescale + 0.5 * (minLat + maxLat)); 
+            double lonRescale = (double) (maxLon - minLon) / 10;
+            double latRescale = (double) (maxLat - minLat) / 10;
+            double newLon = (double) (clickPosition.x * lonRescale + 0.5 * (minLon + maxLon));
+            double newLat = (double) (clickPosition.y * latRescale + 0.5 * (minLat + maxLat)); 
 
             // the angle of the vector from the positive y axis 
-            float theta;
             if (clickPosition.y > 0) 
             {
                 if (clickPosition.x < 0) 
                 {
-                    theta = (float) (Mathf.Atan(clickPosition.x / clickPosition.y) + 6.28);    
+                    theta = (System.Math.Atan(clickPosition.x / clickPosition.y) + 6.28);    
                 }
                 else 
                 {
-                    theta = (float) Mathf.Atan(clickPosition.x / clickPosition.y);
+                    theta = System.Math.Atan(clickPosition.x / clickPosition.y);
                 }
             }
             else 
             {
-                theta = (float) (Mathf.Atan(clickPosition.x / clickPosition.y) + 3.14) ;
-            };
+                theta = (System.Math.Atan(clickPosition.x / clickPosition.y) + 3.14) ;
+            }
+
+            slope = (double) clickPosition.y / clickPosition.x;
 
             // float newX = (float) (clickPosition.x * lonRescale);
             // float newY = (float) (clickPosition.y * latRescale);
@@ -67,11 +70,12 @@ public class ClickPosition : MonoBehaviour
             //     clickPosition = ray.GetPoint(distanceToPlane);
             // }
 
-            Debug.Log("click position is" + clickPosition);
+            // Debug.Log("click position is" + clickPosition);
             Debug.Log("longitutde is " + newLon);
             Debug.Log("latitude is " + newLat);
-            Debug.Log("point vector is" + (clickPosition.x, clickPosition.y));
-            Debug.Log("angle theta is" + theta);
+            // Debug.Log("point vector is" + (clickPosition.x, clickPosition.y));
+            // Debug.Log("angle theta is" + theta);
+            // Debug.Log("slope is " + slope);
         }
     }
 }
---
+++ UnityProject/Assets/NetMQExample/Scripts/ShortestDistance.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShortestDistance : MonoBehaviour
+{
+
+    public GameObject princeLab;
+    public GameObject arnoldLab;
+    public GameObject envLab;
+    public GameObject medicalLab;
+    public GameObject clickDetector;
+
+    public System.String destination; 
+
+
+    // a helper to turn coordinates to angles 
+    double CoordinatesToTheta(double lat, double lon)
+    {
+        if (lat > 0) 
+        {
+            if (lon < 0) 
+            {
+                return System.Math.Atan(lon / lat) + 6.28;    
+            }
+                else 
+            {
+                return System.Math.Atan(lon / lat);
+            }
+        }
+        else 
+        {
+            return System.Math.Atan(lon / lat) + 3.14;
+        };
+    }
+
+    // a helper to calculate the distance between a distance and a line
+    // double DistanceToPoint(double slope, double lat, double lon)
+    // {
+    //     double args1 = System.Math.Abs(lat - (slope * lon));
+    //     double args2 = System.Math.Sqrt(1 + (slope * slope));
+    //     return args1 / args2;
+    // }
+
+    // Start is called before the first frame update
+    // void Start()
+    // {
+    // }
+
+    // Update is called once per frame 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ClickPosition cp = clickDetector.GetComponent<ClickPosition>();
+            double theta = cp.theta;
+            double midLat = 0.5 * (cp.minLat + cp.maxLat);
+            double midLon = 0.5 * (cp.minLon + cp.maxLon);
+            BuildingLocation bd1 = princeLab.GetComponent<BuildingLocation>();
+            double lat1 = bd1.lat - midLat;
+            double lon1 = bd1.lon - midLon;
+            BuildingLocation bd2 = arnoldLab.GetComponent<BuildingLocation>();
+            double lat2 = bd2.lat - midLat;
+            double lon2 = bd2.lon - midLon;
+            BuildingLocation bd3 = envLab.GetComponent<BuildingLocation>();
+            double lat3 = bd3.lat - midLat;
+            double lon3 = bd3.lon - midLon;
+            BuildingLocation bd4 = medicalLab.GetComponent<BuildingLocation>();
+            double lat4 = bd4.lat - midLat;
+            double lon4 = bd4.lon - midLon;
+            double dist1 = System.Math.Abs(CoordinatesToTheta(lat1, lon1) - theta);
+            double dist2 = System.Math.Abs(CoordinatesToTheta(lat2, lon2) - theta);
+            double dist3 = System.Math.Abs(CoordinatesToTheta(lat3, lon3) - theta);
+            double dist4 = System.Math.Abs(CoordinatesToTheta(lat4, lon4) - theta);
+            if (dist1 <= System.Math.Min(dist2, System.Math.Min(dist3, dist4)))
+            {
+                destination = "Prince Engineering Lab";
+            }
+            else 
+            {
+                if (dist2 <= System.Math.Min(dist3, dist4))
+                {
+                    destination = "Arnold Lab";
+                }
+                else 
+                {
+                    if (dist3 <= dist4) 
+                    {
+                        destination = "Urban Environment Lab";
+                    }
+                    else 
+                    {
+                        destination = "Medical Research Lab";
+                    }
+                }
+            }
+            Debug.Log("the closest destination is " + destination);
+        }
+    }
+}
